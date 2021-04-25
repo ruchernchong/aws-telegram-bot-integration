08690b3a97006cb83497f351d379143175d2fe9d
Ru Chern Chong <dev@ruchern.xyz>
2021-04-25 20:36:05 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const dotEnv = require('dotenv');
+const { Telegraf } = require('telegraf');
+
+dotEnv.config();
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+/**
+ * Send the message to telegram based on the chat id
+ */
+const sendMessageToTelegram = async message => {
+  console.log(`CHAT_ID`, process.env.TELEGRAM_CHAT_ID);
+  console.log(`message`, message);
+
+  await bot.telegram.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
+};
+
+/**
+ *
+ * @return {Promise<void>}
+ */
+module.exports.handler = async () => {
+  await bot.launch();
+
+  await console.log(`Bot started at:`, Date.now());
+
+  await sendMessageToTelegram(
+    'This is an interval test message for AWS integration'
+  ).then(() => console.info('Message sent!'));
+
+  await bot.stop('Terminating the bot');
+};
